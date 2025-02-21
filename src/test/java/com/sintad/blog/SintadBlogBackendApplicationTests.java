3d5fec2a3bdd73b280157ff0be7c0b239f39c651
NyoByte <andy.nyog@gmail.com>
2025-02-21 20:39:37 +0100

    initial commit

---
+++ src/test/java/com/sintad/blog/SintadBlogBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sintad.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SintadBlogBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
