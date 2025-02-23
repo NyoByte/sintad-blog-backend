32425eab0a3c8077a27d48a7d0e8a48aab760f5b
NyoByte <andy.nyog@gmail.com>
2025-02-23 07:12:05 +0100

    cambios docker compose

--- src/main/java/com/sintad/blog/config/SecurityConfig.java
+++ src/main/java/com/sintad/blog/config/SecurityConfig.java
@@ -66,7 +66,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(List.of("*"));
-        configuration.setAllowedOrigins(List.of("http://localhost:4300", "http://localhost:80"));
+        configuration.setAllowedOrigins(List.of("http://localhost:8080", "http://localhost:4300", "http://localhost:80"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("Authorization", "Content-Type"));
         configuration.setAllowCredentials(true);
